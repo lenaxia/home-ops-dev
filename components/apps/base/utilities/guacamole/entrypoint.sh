815fb9d635e30bd3983be71c7ff27a2fa0b9e776
Mike Kao <publicmike@outlook.com>
2022-09-11 05:15:21 +0000

    blkjdf

--- components/apps/base/utilities/guacamole/entrypoint.sh
+++ components/apps/base/utilities/guacamole/entrypoint.sh
@@ -3,7 +3,11 @@ apt update && apt upgrade
 apt install -y software-properties-common gnupg sudo wget
 sudo bash -c 'echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list.d/backports.list'
 sudo apt update && sudo apt -y -t buster-backports install freerdp2-dev libpulse-dev freerdp2-x11
-wget https://dlcdn.apache.org/guacamole/1.4.0/binary/guacamole-auth-header-1.4.0.tar.gz /home/guacamole/.guacamole/extensions/
+#wget https://dlcdn.apache.org/guacamole/1.4.0/binary/guacamole-auth-header-1.4.0.tar.gz /home/guacamole/.guacamole/extensions/
+mkdir /config/extensions
+wget https://dlcdn.apache.org/guacamole/1.4.0/binary/guacamole-auth-header-1.4.0.tar.gz /config/extensions/
 
-sudo bash -c 'echo "http-auth-header: Remote-User" >> /home/guacamole/.guacamole/guacamole.properties'
-sudo bash -c 'echo "testing123" >> /config/guacamole.properties'
+
+#sudo bash -c 'echo "http-auth-header: Remote-User" >> /home/guacamole/.guacamole/guacamole.properties'
+bash -c 'echo "http-auth-header: Remote-User" >> /config/guacamole.properties'
+#sudo bash -c 'echo "testing123" >> /config/guacamole.properties'
