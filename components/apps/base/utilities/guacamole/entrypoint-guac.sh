b90184eb7613402e342942f1e8b3c208fb9d3539
Mike Kao <publicmike@outlook.com>
2022-09-11 06:02:09 +0000

    sdf

--- components/apps/base/utilities/guacamole/entrypoint-guac.sh
+++ components/apps/base/utilities/guacamole/entrypoint-guac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update && apt upgrade
-apt install -y software-properties-common gnupg sudo wget
+#apt update && apt upgrade
+#apt install -y software-properties-common gnupg sudo wget
 mkdir /config/extensions
 wget https://dlcdn.apache.org/guacamole/1.4.0/binary/guacamole-auth-header-1.4.0.tar.gz /config/extensions/
 
