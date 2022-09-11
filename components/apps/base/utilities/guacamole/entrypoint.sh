4d0eef08c288bfe181427c0f888502abfc2818a2
Mike Kao <publicmike@outlook.com>
2022-09-11 04:37:21 +0000

    blah

--- components/apps/base/utilities/guacamole/entrypoint.sh
+++ components/apps/base/utilities/guacamole/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update && apt upgrade
-apt install software-properties-common gnupg sudo
+apt install -y software-properties-common gnupg sudo wget
 sudo bash -c 'echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list.d/backports.list'
 sudo apt update && sudo apt -y -t buster-backports install freerdp2-dev libpulse-dev freerdp2-x11
 wget https://dlcdn.apache.org/guacamole/1.4.0/binary/guacamole-auth-header-1.4.0.tar.gz /home/guacamole/.guacamole/extensions/
