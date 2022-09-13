294f732cfe86431d27f565951bc8e2c3c1b22a0b
Mike Kao <publicmike@outlook.com>
2022-09-13 04:08:50 +0000

    guacliveliness

--- components/apps/base/utilities/guacamole/entrypoint-guac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#apt update && apt upgrade
-#apt install -y software-properties-common gnupg sudo wget
-mkdir /config/extensions
-wget https://dlcdn.apache.org/guacamole/1.4.0/binary/guacamole-auth-header-1.4.0.tar.gz /config/extensions/
-
-bash -c 'echo "http-auth-header: Remote-User" >> /config/guacamole.properties'
