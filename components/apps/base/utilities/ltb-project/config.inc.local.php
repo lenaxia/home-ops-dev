5fe48375fd8f857d8a795550179ef693ade2d299
Mike Kao <publicmike@outlook.com>
2022-09-22 17:33:58 +0000

    ltb

--- components/apps/base/utilities/ltb-project/config.inc.local.php
+++ components/apps/base/utilities/ltb-project/config.inc.local.php
@@ -2,10 +2,10 @@
 $keyphrase = "PTRhHOCi6xdxxMMFbDJKjbaLeHaTklv3PFUz9W9wgMGyQPXX1zJOOHyiK4ZT3Nl0";
 $debug = true;
 $ldap_url = "ldap://192.168.0.120:389";
-#$ldap_binddn = "uid=pwm-admin,cn=users,dc=kao,dc=family";
-#$ldap_bindpw = "9S&xyqE8AQ&b&z";
-$ldap_binddn = "uid=root,cn=users,dc=kao,dc=family";
-$ldap_bindpw = "faith6510";
+$ldap_binddn = "uid=ltb-admin,cn=users,dc=kao,dc=family";
+$ldap_bindpw = "9S&xyqE8AQ&b&z";
+#$ldap_binddn = "uid=root,cn=users,dc=kao,dc=family";
+#$ldap_bindpw = "faith6510";
 $ldap_base = "dc=kao,dc=family";
 $ldap_filter = "(&(objectClass=posixAccount)(uid={login}))";
 $who_change_password = "user";
