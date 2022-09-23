f32b1d01edc3413a8e0b79fa45ea6221ec5d4029
Mike Kao <publicmike@outlook.com>
2022-09-23 09:52:57 +0000

    blah

--- components/apps/base/utilities/ltb-project/config.inc.local.php
+++ components/apps/base/utilities/ltb-project/config.inc.local.php
@@ -2,9 +2,9 @@
 $keyphrase = getenv('SECRET_LTB_KEYPHRASE');
 $debug = true;
 $ldap_url = "ldap://192.168.0.120:389";
-$ldap_binddn = getenv('SECRET_LTB_LDAP_BINDDN');
-$ldap_bindpw = getenv('SECRET_LTB_LDAP_PASSWORD');
-$ldap_base = getenv('SECRET_LTB_LDAP_BASE');
+$ldap_binddn = "getenv('SECRET_LTB_LDAP_BINDDN')";
+$ldap_bindpw = "getenv('SECRET_LTB_LDAP_PASSWORD')";
+$ldap_base = "getenv('SECRET_LTB_LDAP_BASE')";
 $ldap_filter = "(&(objectClass=posixAccount)(uid={login}))";
 $who_change_password = "user";
 
