b92519ce780ec3ad0d46d02c8a5a6fb55f1c8252
Mike Kao <publicmike@outlook.com>
2022-10-03 14:16:43 +0000

    ltb

--- components/apps/base/utilities/ltb-project/postStart.sh
+++ components/apps/base/utilities/ltb-project/postStart.sh
@@ -2,6 +2,6 @@
 
 apt update
 apt install wget
-wget "http://192.168.0.120:9000/public/customlogo.png" -O /var/www/html/images/custom-logo.png
-wget "http://192.168.0.120:9000/public/favicon.ico" -O /var/www/html/images/favicon.ico
+wget "https://${SECRET_DEV_DOMAIN}/public/customlogo.png" -O /var/www/html/images/custom-logo.png
+wget "https://s3.${SECRET_DEV_DOMAIN}/public/favicon.ico" -O /var/www/html/images/favicon.ico
 
