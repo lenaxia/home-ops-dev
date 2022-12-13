ab524d27ac0f7e6cac2cc89ae2e0cb924606902d
Mike Kao <publicmike@outlook.com>
2022-12-13 23:39:19 +0000

    blah

--- cluster/apps/utilities/vaultwarden_ldap/postStart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt update
-apt install wget
-wget "https://s3.${SECRET_DEV_DOMAIN2}/public/customlogo.png" -O /var/www/html/images/custom-logo.png
-wget "https://s3.${SECRET_DEV_DOMAIN2}/public/favicon.ico" -O /var/www/html/images/favicon.ico
-
